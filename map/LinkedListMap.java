3943d204ab52028ec76116db5e3dabbcb2e87e3d
Chip <963255218@qq.com>
2023-08-02 16:14:30 +0800

    基于链表的Map实现

---
+++ map/LinkedListMap.java
@@ -0,0 +1,140 @@
+package com.algorithm.map;
+
+import com.algorithm.set.FileOperation;
+
+import java.util.ArrayList;
+
+
+/**
+ * author : Chip
+ * time   : 2023/8/2
+ * desc   : 基于链表的Map实现
+ */
+public class LinkedListMap<K, V> implements Map<K, V> {
+
+    private class Node {
+        public K key;
+        public V value;
+        public Node next;
+
+        public Node(K key, V value, Node next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+
+        public Node(K key) {
+            this(key, null, null);
+        }
+
+        public Node() {
+            this(null, null, null);
+        }
+
+        @Override
+        public String toString() {
+            return key.toString() + " : " + value.toString();
+        }
+    }
+
+    private Node dummyHead;
+    private int size;
+
+    public LinkedListMap() {
+        dummyHead = new Node();
+        size = 0;
+    }
+
+    @Override
+    public void add(K key, V value) {
+        Node node = getNode(key);
+        if (node == null) {
+            dummyHead.next = new Node(key, value, dummyHead.next);
+            size++;
+        } else {
+            node.value = value;
+        }
+    }
+
+    @Override
+    public V remove(K key) {
+
+        Node prev = dummyHead;
+        while (prev.next != null) {
+            if (prev.next.key.equals(key)) {
+                break;
+            }
+            prev = prev.next;
+        }
+        if (prev.next != null) {
+            Node delNode = prev.next;
+            prev.next = delNode.next;
+            //待删除节点不再指向任何节点，从链表中脱离
+            delNode.next = null;
+            size--;
+            return delNode.value;
+        }
+
+        return null;
+    }
+
+    @Override
+    public boolean contains(K key) {
+        return getNode(key) != null;
+    }
+
+    @Override
+    public V get(K key) {
+        Node node = getNode(key);
+        return node == null ? null : node.value;
+    }
+
+    @Override
+    public void set(K key, V newValue) {
+        Node node = getNode(key);
+        if (node == null) {
+            throw new IllegalArgumentException(key + "does not exist!");
+        } else {
+            node.value = newValue;
+        }
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    private Node getNode(K key) {
+        Node cur = dummyHead.next;
+        while (cur != null) {
+            if (cur.key.equals(key)) {
+                return cur;
+            }
+            cur = cur.next;
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("fairyTale.txt");
+        ArrayList<String> words = new ArrayList<>();
+        if (FileOperation.readFile("fairyTale.txt", words)) {
+            System.out.println("Total words: " + words.size());
+            LinkedListMap<String, Integer> map = new LinkedListMap<>();
+            for (String word : words) {
+                if (map.contains(word)) {
+                    map.set(word, map.get(word) + 1);
+                } else {
+                    map.add(word, 1);
+                }
+            }
+            System.out.println("Total Difference words: " + map.size());
+            System.out.println("Frequency of and:" + map.get("and"));
+        }
+    }
+}
---
+++ map/Main.java
@@ -0,0 +1,9 @@
+package com.algorithm.map;
+
+/**
+ * author : Chip
+ * time   : 2023/8/2
+ * desc   :
+ */
+public class Main {
+}
