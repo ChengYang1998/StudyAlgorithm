2ff57bf977c449599fc3e130172153cec1f41592
Chip <963255218@qq.com>
2023-08-14 14:26:09 +0800

    最大堆基础

---
+++ maxheap/MaxHeap.java
@@ -0,0 +1,57 @@
+package com.algorithm.maxheap;
+
+import com.algorithm.array.Array;
+
+/**
+ * author : Chip
+ * time   : 2023/8/14
+ * desc   :
+ */
+public class MaxHeap<E extends Comparable<E>> {
+
+    private Array<E> data;
+
+    public MaxHeap(int capacity) {
+        data = new Array<>(capacity);
+    }
+
+    public MaxHeap() {
+        data = new Array<>();
+    }
+
+    public int size() {
+        return data.getSize();
+    }
+
+    public boolean isEmpty() {
+        return data.isEmpty();
+    }
+
+    /**
+     * @param index 索引
+     * @return 返回完全二叉树的数组表示中，一个索引所表示的元素的父亲节点的索引
+     */
+    private int parent(int index) {
+        if (index == 0) {
+            throw new IllegalArgumentException("index-0 does not have parent.");
+        }
+        return (index - 1) / 2;
+    }
+
+    /**
+     * @param index 索引
+     * @return 返回完全二叉树的数组表示中，一个索引所表示的元素的左孩子节点的索引
+     */
+    private int leftChild(int index) {
+        return index * 2 + 1;
+    }
+
+    /**
+     * @param index 索引
+     * @return 返回完全二叉树的数组表示中，一个索引所表示的元素的右孩子节点的索引
+     */
+    private int rightChild(int index) {
+        return index * 2 + 2;
+    }
+
+}
